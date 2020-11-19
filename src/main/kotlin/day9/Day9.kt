912a9ef0e562a7df75faaf490a7f3da728bf8e14
Joozd <joozd@ius.org>
2020-11-19 19:23:48 +0100

    day 9

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -37,10 +37,6 @@ class Day9(day: Int): Solution(day) {
         }
         return result.joinToString("")
     }
-
-    companion object {
-        const val test = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
-    }
 }
 
 
